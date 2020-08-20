1c7131a47872ba4b5ea403b7634df5379a08cb2b
claire froelich <claire.froelich@gmail.com>
2020-08-20 13:39:16 -0700

    refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 // import { Link, Route, Router } from "react-router-dom";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import { Home } from "./views/pages";
-// import routes from "./routes";
+// import { Home } from "./views/pages";
+import { routes } from "./routes";
 
 import "./styles/index.css";
 
 const App: React.FC<Element> = () => (
   <Router>
-    <Route exact path="/" component={Home} />
+    {routes.map((route) => (
+      <Route key={route.path} {...route} />
+    ))}
   </Router>
 );
 
 export default App;
-
-// { routes.map( route => (
-//     <Route key={ route.path } { ...route } />
-// ) ) }
--- src/routes.ts
+++ src/routes.ts
@@ -1,1 +1,15 @@
-export default [{ path: "something" }];
+import { Home } from "./views/pages";
+
+interface RouteConfig {
+  component: any;
+  exact: boolean;
+  path: string;
+}
+
+export const routes: RouteConfig[] = [
+  {
+    component: Home,
+    exact: true,
+    path: "/",
+  },
+];
