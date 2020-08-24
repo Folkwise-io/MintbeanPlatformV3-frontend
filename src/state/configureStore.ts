def1b8a71caa91d2a6acb918f23b6bb556933006
claire froelich <claire.froelich@gmail.com>
2020-08-24 10:59:53 -0700

    refactor store

---
+++ src/state/configureStore.ts
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, Action } from "redux";
+import logger from "./middleware/logger";
+import thunkMiddleware from "redux-thunk";
+import { rootReducer } from "./reducers";
+import { Bean } from "../types/Bean";
+
+export interface StoreState {
+  beans: Bean[];
+}
+
+const initialState: StoreState = {
+  beans: [
+    { id: 1, username: "clairefro", body: "this is bean 1", createdAt: new Date() },
+    { id: 2, username: "clairefro", body: "this is bean 2", createdAt: new Date() },
+  ],
+};
+
+// TODO: properly type configureStore
+export default function configureStore(): any {
+  const middlewares = [logger, thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  // TODO: properly type createStore
+  const store = createStore<StoreState, Action, unknown, unknown>(rootReducer, initialState, middlewareEnhancer);
+
+  return store;
+}
