42ed985f28d6af5709efc5756768395e32a783c6
claire froelich <claire.froelich@gmail.com>
2020-08-28 15:46:57 -0700

    Fix minor typo

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { fetchPostsByUsername } from "../../services/postService";
 import { fetchUsers } from "../../state/actions/userActions";
 import { StoreState } from "../../state/types";
-import LuanchesContainer from "../features/Launches/LaunchesContainer";
+import LaunchesContainer from "../features/Launches/LaunchesContainer";
 import UserListComponent from "../features/UserList/UserListComponent";
 import { User } from "../../types/User";
 import { Post } from "../../types/Post";
@@ -40,7 +40,7 @@ const Home: FC<HomeProps> = ({ storeUsers, dispatchFetchUsers }: HomeProps) => {
 
   return (
     <div className="container mx-auto max-w-screen-md">
-      <LuanchesContainer />
+      <LaunchesContainer />
 
       <h2>Users</h2>
       <UserListComponent users={storeUsers} />
