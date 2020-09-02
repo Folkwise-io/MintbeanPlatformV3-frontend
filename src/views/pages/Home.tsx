81743c85b0144977220876b2312831415ce3a344
Monarch Wadia <monarch@mintbean.io>
2020-09-02 00:04:08 -0400

    Cleaned up Home.tsx

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -39,7 +39,6 @@ const Home: FC<HomeProps> = ({ storeUsers, dispatchFetchUsers }: HomeProps) => {
 
   return (
     <div className="container mx-auto max-w-screen-md">
-
       <h2>Users</h2>
       <UserListComponent users={storeUsers} />
       <br />
@@ -64,49 +63,3 @@ const Home: FC<HomeProps> = ({ storeUsers, dispatchFetchUsers }: HomeProps) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
-
-// const handleSubmit = (e: any): void => {
-//   e.preventDefault();
-//   console.log(data);
-//   dispatchCreatePost(data);
-// };
-//
-// const changeHandler = (event: any) => {
-//   event.persist();
-//
-//   const value = event.target.value;
-//
-//   setData((prevState) => ({
-//     ...prevState,
-//     [event.target.name]: value,
-//   }));
-// };
-//
-// ...
-
-// <form onSubmit={handleSubmit} className="flex flex-col">
-//   <input
-//     type="text"
-//     value={data.username}
-//     name="username"
-//     onChange={changeHandler}
-//     placeholder="username"
-//     className="mb-2 p-2 border-2 border-solid border-gray-500"
-//   />
-//
-//   <textarea
-//     name="body"
-//     value={data.body}
-//     placeholder="body"
-//     onChange={changeHandler}
-//     className="mb-2 p-2 border-2 border-solid border-gray-500"
-//   />
-//   <input type="submit" value="add post" className="mb-2 p-2" />
-// </form>
-// <h2>have some posts</h2>
-// <ul>
-//   {posts.map((b) => (
-//     <PostContainer key={b.id} post={b} />
-//   ))}
-// </ul>
-// </div>
