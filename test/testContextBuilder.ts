5992212914a1aee6100217be6b225bd008a991ce
kmbarnett <kmbarnett94@gmail.com>
2020-09-06 07:03:35 -0600

    Fix typing error

--- test/testContextBuilder.ts
+++ test/testContextBuilder.ts
@@ -1,5 +1,6 @@
 import { ApiQueryExecutor } from "../src/api/ApiQueryExecutor";
 import { UserService } from "../src/services/userService";
+import { EventService } from "../src/services/eventService";
 import { TestUserDao } from "./src/TestUserDao";
 import { TestEventDao } from "./src/TestEventDao";
 
@@ -8,15 +9,18 @@ export interface TestContext {
   userDao: TestUserDao;
   eventDao: TestEventDao;
   userService: UserService;
+  eventService: EventService;
 }
 
 export const testContextBuilder = (): TestContext => {
   const userDao = new TestUserDao();
   const eventDao = new TestEventDao();
   const userService = new UserService(userDao);
+  const eventService = new EventService(eventDao);
 
   return {
     apiQueryExecutor: new ApiQueryExecutor(),
+    eventService,
     userDao,
     eventDao,
     userService,
