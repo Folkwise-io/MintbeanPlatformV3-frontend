7f515e3e8bf8675aedac717b563361192edb9493
kmbarnett <kmbarnett94@gmail.com>
2020-09-06 19:24:51 -0600

    Add Triangles to tooltips

--- src/views/components/stories/Tooltip.stories.jsx
+++ src/views/components/stories/Tooltip.stories.jsx
@@ -10,7 +10,7 @@ export default {
 const Template = (args) => (
   <div className="w-full flex justify-center h-64 items-center">
     <Tooltip duration={args.duration} tip={args.tip} direction={args.direction} delay={args.delay}>
-      <button>Hover Over Me</button>
+      <button className="px-2 py-1 border-green-400 rounded-lg border-2 bg-green-300">Hover Over Me</button>
     </Tooltip>
   </div>
 );
