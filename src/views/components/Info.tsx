2353defe263afbb78834b3698eb5b30f7306aaef
kmbarnett <kmbarnett94@gmail.com>
2020-09-08 13:39:03 -0600

    Add tailwind styles

--- src/views/components/Info.tsx
+++ src/views/components/Info.tsx
@@ -1,14 +1,24 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import infoIcon from "../../assets/info-icon.png";
 export type InfoProps = {
   info: string;
 };
 
-export const Info: FC<InfoProps> = ({ info }) => {
+export const Info: FC<InfoProps> = ({ info, children }) => {
+  const [infoModal, openCloseInfo] = useState(false);
+
   return (
-    <label htmlFor="info-check" data-info={info}>
-      <input type="checkbox" id="info-check" />
-      <img src={infoIcon} alt="click to see more information"/>
-    </label>
+    <div>
+      <label htmlFor="info-check" data-info={info}>
+        <input
+          type="checkbox hidden"
+          defaultChecked={infoModal}
+          onChange={() => openCloseInfo(!infoModal)}
+          id="info-check"
+        />
+        <img className="pointer-events-none" src={infoIcon} alt="click to see more information" />
+      </label>
+      {infoModal && <section>{children}</section>}
+    </div>
   );
 };
