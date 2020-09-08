113d44b11e2bf7700e90410f17dad16e18abc977
kmbarnett <kmbarnett94@gmail.com>
2020-09-08 15:59:30 -0600

    Fix timeout

--- src/views/components/wrappers/Tooltip.tsx
+++ src/views/components/wrappers/Tooltip.tsx
@@ -10,21 +10,24 @@ export type TooltipProps = {
 
 export const Tooltip: FC<TooltipProps> = ({ duration, delay = 0, message, direction = "", children }) => {
   const [hidden, hide] = useState(true);
+  let durTimeOut: number;
+  let delayTimeout: number;
 
   const handleDuration = () => {
     if (duration) {
-      setTimeout(hide, delay + duration, true);
+      durTimeOut = setTimeout(hide, delay + duration, true);
     }
   };
 
   const handleHover = () => {
-    setTimeout(hide, delay, false);
+    delayTimeout = setTimeout(hide, delay, false);
     handleDuration();
   };
 
   const handleUnHover = () => {
     hide(true);
-    clearTimeout();
+    clearTimeout(delayTimeout);
+    clearTimeout(durTimeOut);
   };
 
   const classes = direction ? `tooltip--${direction}` : "tooltip";
