09bb475776d963d649d7ef18db3ff8bb3d93961d
Monarch Wadia <monarch@mintbean.io>
2020-09-08 14:03:46 -0400

    Fixed connectContext typescript issues.

--- src/context/connectContext.tsx
+++ src/context/connectContext.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, ReactElement, ComponentType } from "react";
 import { Context } from "./contextBuilder";
 import { MbContext } from "./MbContext";
 
 export interface ConnectContextProps {
-  context: Context | undefined;
+  context?: Context | undefined;
 }
 
-export function connectContext<P extends ConnectContextProps>(TheComponent: React.FC<P>) {
-  return function ContextWrapper(props: P): JSX.Element {
+export function connectContext<P extends ConnectContextProps>(TheComponent: ComponentType<P>) {
+  return function ContextWrapper(props: P): ReactElement<P> {
     const context = useContext(MbContext);
     return <TheComponent context={context} {...props} />;
   };
