c529de1333876abc1a2b11b23169814c7cde4381
kmbarnett <kmbarnett94@gmail.com>
2020-09-09 11:01:46 -0600

    Remove zones from sample data

--- src/views/components/stories/EventCard.stories.jsx
+++ src/views/components/stories/EventCard.stories.jsx
@@ -17,8 +17,8 @@ export const FirstStory = Template.bind({});
 FirstStory.args = {
   name: "Test Event Title",
   description: testDescription,
-  startDate: "Mon, 14 Sep 2020 07:00:00 GMT",
-  endDate: "Thu, 17 Sep 2020 07:00:00 GMT",
+  startDate: "Mon, 14 Sep 2020 07:00:00",
+  endDate: "Thu, 17 Sep 2020 07:00:00",
   image: "https://www.kylebar.net/assets/images/ProfilePic-667fae86635de9cc40d293b47f9a1047.jpg",
   region: "America/Toronto",
 };
