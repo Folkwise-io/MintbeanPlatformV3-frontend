ab370fdee6e972d051799eff35a6c9d84f3db73b
kmbarnett <kmbarnett94@gmail.com>
2020-09-11 13:10:30 -0600

    style project card

---
+++ src/views/components/stories/ProjectCard.stories.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ProjectCard } from "../ProjectCard";
+import { MemoryRouter as Router } from "react-router-dom";
+
+// This default export determines where you story goes in the story list
+export default {
+  title: "ProjectCard",
+  component: ProjectCard,
+};
+
+const Template = (args) => {
+  const project = {};
+  project.user = {};
+  for (const key in args) {
+    if (key.includes("Name")) {
+      project.user[key] = args[key];
+    } else {
+      project[key] = args[key];
+    }
+  }
+  console.log(project);
+  return (
+    <Router>
+      <div className="w-full flex justify-center items-center">
+        <ProjectCard project={project} />
+      </div>
+    </Router>
+  );
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  title: "Test Project Title",
+  image: "https://media.giphy.com/media/iwVHUKnyvZKEg/giphy.gif",
+  id: "testID",
+  sourceCodeUrl: "https://github.com/Mintbean/MintbeanPlatformV3-frontend",
+  liveUrl: "https://mintbean-spacegame.herokuapp.com/",
+  firstName: "John",
+  lastName: "Snow",
+};
