59ddba9653ce8b45bdbbd018401cec56d6357bf3
claire froelich <claire.froelich@gmail.com>
2020-09-16 12:18:50 -0700

    Simplify button component

--- src/views/components/Button.tsx
+++ src/views/components/Button.tsx
@@ -6,14 +6,14 @@ type Props = {
 
 export const Button: FC<Props> = (props) => {
   const { type = "primary", children, ...rest } = props;
-  const commonClasses = "shadow-md py-2 px-6 rounded-lg hover:shadow-sm border-2 border-solid";
+  const common = "shadow-md py-2 px-6 rounded-lg hover:shadow-sm border-2 border-solid";
   const classes = {
     primary: "text-white bg-mb-mint border-mb-mint",
     secondary: "text-gray-700 bg-white border-mb-mint",
     danger: "text-white bg-red-500 border-red-500",
   };
   return (
-    <button {...rest} className={`${commonClasses} ${classes[type]}`}>
+    <button {...rest} className={`${common} ${classes[type]}`}>
       {children}
     </button>
   );
