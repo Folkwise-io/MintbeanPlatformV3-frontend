de5d5a13b2bc7e0e3ef2a2fffd989c80eb33806b
claire froelich <claire.froelich@gmail.com>
2020-09-16 13:23:03 -0700

    Remove logger from MeetService

--- src/services/meetService.ts
+++ src/services/meetService.ts
@@ -1,11 +1,9 @@
 import { MeetDao } from "../daos/MeetDao";
-import { LoggerService } from "./loggerService";
 
 export class MeetService {
-  constructor(private meetDao: MeetDao, private logger: LoggerService) {}
+  constructor(private meetDao: MeetDao) {}
 
-  /* This action does not update the redux store, so error handling happens here in service rather than an action */
-  async fetchMeets(): Promise<HackMeet[] | void> {
-    return this.meetDao.fetchMeets().catch((e) => this.logger.handleGraphqlErrors(e));
+  async fetchMeets(): Promise<HackMeet[]> {
+    return this.meetDao.fetchMeets();
   }
 }
