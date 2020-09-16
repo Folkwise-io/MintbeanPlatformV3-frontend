049d34428ae6bdaca6a5d69312dfef21ff71fd6c
claire froelich <claire.froelich@gmail.com>
2020-09-15 21:23:32 -0700

    Appease linter gods

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,6 +1,8 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 export const isServerError = (tbd: any) => {
   if (tbd as ServerError) {
     return true;
   }
   return false;
 };
+/* eslint-enable  @typescript-eslint/no-explicit-any */
