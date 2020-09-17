5bf4fad5714015e90a6040a839ebee519923a503
claire froelich <claire.froelich@gmail.com>
2020-09-16 19:34:22 -0700

    Simplify MeetCard css

--- src/views/components/MeetCard.tsx
+++ src/views/components/MeetCard.tsx
@@ -14,13 +14,14 @@ export const MeetCard: FC<MeetProps> = ({ meet }) => {
 
   const startTimeStr = d.wcToClientStr(startTime, region, { clientRegion: undefined });
   const duration = d.getDuration(startTime, endTime);
+
   return (
     <div className="shadow-md bg-white flex flex-col md:flex-row max-w-screen-md items-center rounded-lg overflow-hidden">
-      <div className="max-w-full md:w-1/3 md:max-h-full">
-        <img className="w-full h-full object-cover" src={coverImageUrl} alt={`${title} event banner`}></img>
+      <div className="max-w-full md:w-1/3 ">
+        <img className="object-cover" src={coverImageUrl} alt={`${title} event banner`}></img>
       </div>
-      <div className="w-2/3 p-4">
-        <section className="my-1 w-full">
+      <div className="w-2/3 py-4 md:p-4">
+        <section className="flex flex-col my-1 w-full">
           <h2 className="text-2xl mb-2">{title}</h2>
           <p className="mb-2">{description}</p>
           <p className="mb-2">
