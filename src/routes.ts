6201a40f56735cc3c25569ab8b59dbf61ba9b78d
theFl00f <celeste.ellerby@gmail.com>
2020-09-18 18:19:11 -0400

    removed import of head

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home, Login, StaticHome, Hackathons, NotFound, Meets } from "./views/pages";
+import { Login, StaticHome, Hackathons, NotFound, Meets } from "./views/pages";
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 interface RouteConfig {
