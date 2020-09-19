6ad6dc39c8447802b604c4ed2c11cc7e90a8cdc8
claire froelich <claire.froelich@gmail.com>
2020-09-19 10:33:41 -0700

    Add webpackconfig for using environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,97 @@
+const webpack = require("webpack");
 const path = require("path");
+const dotenv = require("dotenv");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-  // webpack will take the files from ./src/index
-  entry: "./src/index",
-  devtool: "source-map",
 
-  // and output it into /dist as bundle.js
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
+const configEnvVariables = () => {
+  const env = dotenv.config().parsed;
 
-  // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return envKeys;
+};
 
-  module: {
-    rules: [
-      // we use babel-loader to load our jsx and tsx files
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    targets: {
-                      node: "12",
+module.exports = () => {
+  const envKeys = configEnvVariables();
+  return {
+    // webpack will take the files from ./src/index
+    entry: "./src/index",
+    devtool: "source-map",
+
+    // and output it into /dist as bundle.js
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "bundle.js",
+    },
+
+    // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+
+    module: {
+      rules: [
+        // we use babel-loader to load our jsx and tsx files
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  [
+                    "@babel/preset-env",
+                    {
+                      targets: {
+                        node: "12",
+                      },
                     },
-                  },
+                  ],
+                  "@babel/preset-react",
                 ],
-                "@babel/preset-react",
-              ],
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
 
-      // asset loading
-      {
-        test: /\.(jpe?g|gif|png|svg)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
+        // asset loading
+        {
+          test: /\.(jpe?g|gif|png|svg)$/i,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 10000,
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
 
-      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      { test: /\.css$/, loader: "postcss-loader" },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    // headers: {
-    //   "Access-Control-Allow-Origin": "*",
-    //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    //   "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
-    // },
-    proxy: {
-      "/graphql": {
-        target: "http://localhost:4000",
+        // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        { test: /\.css$/, loader: "postcss-loader" },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+      proxy: {
+        "/graphql": {
+          target: "http://localhost:4000",
+        },
       },
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      // set up environment variables
+      new webpack.DefinePlugin(envKeys),
+    ],
+  };
 };
