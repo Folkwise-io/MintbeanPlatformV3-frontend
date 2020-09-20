6b88b93591432eefeefac439e954c1cfd7181c46
theFl00f <celeste.ellerby@gmail.com>
2020-09-20 13:17:44 -0400

    added icons and react-fontawesome dependency

--- src/views/components/SocialLinks.tsx
+++ src/views/components/SocialLinks.tsx
@@ -1,1 +1,25 @@
 import React, { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+export const SocialLinks: FC<unknown> = () => {
+  const commonTarget = "_blank";
+  const commonRel = "noopener noreferrer";
+
+  return (
+    <div>
+      <a rel={commonRel} target={commonTarget} href="https://www.linkedin.com/company/mintbean/">
+        <span className="sr-only">Mintbean - Twitter</span>
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </a>
+      <a rel={commonRel} target={commonTarget} href="https://www.linkedin.com/company/mintbean/">
+        <span className="sr-only">Mintbean - Twitter</span>
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <a rel={commonRel} target={commonTarget} href="https://www.facebook.com/Mintbean-104353817855570">
+        <span className="sr-only">Mintbean - Facebook</span>
+        <FontAwesomeIcon icon={faFacebook} />
+      </a>
+    </div>
+  );
+};
--- src/views/layouts/Footer.tsx
+++ src/views/layouts/Footer.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
+import { SocialLinks } from "../components/SocialLinks";
 import { DiscordButton } from "../components/Footer/DiscordButton";
 
 type Footer = {
@@ -13,14 +14,15 @@ type Props = {
 
 export const Footer: FC<Props> = ({ footer }) => {
   const { footerNav, links } = footer;
-  const buttonArg = {
+  const buttonArgs = {
     text: "Come join us!",
   };
+
   return (
     <footer className="w-full font-body text-sm grid grid-rows-2 md:grid-cols-3 md:grid-rows-1 items-stretch md:pt-24 pb-12 md:gap-4 lg:grid-cols-2 lg:gap-0">
       <article className="mx-10 mb-10 md:mb-0 md:grid md:grid-rows-2 md:col-span-1 lg:grid-rows-1 lg:grid-cols-2 md:mr-0 md:ml-12">
-        <DiscordButton button={buttonArg} />
-        <div></div>
+        <DiscordButton button={buttonArgs} />
+        <SocialLinks />
       </article>
       <article className="flex flex-col justify-between md:col-span-2 lg:col-span-1">
         <ul className={`grid grid-cols-1 md:flex md:justify-evenly md:w-full`}>
