18d13752c1725ac9388e0a669e6f6967eaef42fc
claire froelich <claire.froelich@gmail.com>
2020-09-21 13:28:17 -0700

    Add half-ass factory for projects

--- test/src/factories/project.factory.ts
+++ test/src/factories/project.factory.ts
@@ -1,15 +1,35 @@
 import faker from "faker";
-import { factory } from "./factory";
+// import { factory } from "./factory";
 
-export const meetProjectFactory = factory<ProjectForMeet>({
-  id: () => faker.random.uuid(),
-  title: () => faker.company.bs(),
-  sourceCodeUrl: () => faker.internet.url(),
-  liveUrl: () => faker.internet.url(),
+// TODO: make this work with actual factory method (allow nested assocaitions via recursive object merging)
+
+// export const meetProjectFactory = factory<ProjectForMeet>({
+//   id: () => faker.random.uuid(),
+//   title: () => faker.company.bs(),
+//   sourceCodeUrl: () => faker.internet.url(),
+//   liveUrl: () => faker.internet.url(),
+//   user: {
+//     firstName: () => faker.name.firstName(),
+//     lastName: () => faker.name.lastName(),
+//     username: () => faker.random.words().replace(/[\s-]/g, "").toLowerCase() + faker.finance.mask(),
+//   },
+//   mediaAssets: [{ cloudinaryPublicId: "bo3bpbanohqsbf3bzc9c" }, { cloudinaryPublicId: "rtcx50vsggqq9wjvhob6" }],
+// });
+
+const fakeProject = {
+  id: faker.random.uuid(),
+  title: faker.company.bs(),
+  sourceCodeUrl: faker.internet.url(),
+  liveUrl: faker.internet.url(),
   user: {
-    firstName: () => faker.name.firstName(),
-    lastName: () => faker.name.lastName(),
-    username: () => faker.random.words().replace(/[\s-]/g, "").toLowerCase() + faker.finance.mask(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    username: faker.random.words().replace(/[\s-]/g, "").toLowerCase() + faker.finance.mask(),
   },
   mediaAssets: [{ cloudinaryPublicId: "bo3bpbanohqsbf3bzc9c" }, { cloudinaryPublicId: "rtcx50vsggqq9wjvhob6" }],
-});
+};
+
+export const meetProjectFactory = {
+  one: () => fakeProject,
+  bulk: () => [fakeProject],
+};
