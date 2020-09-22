d644240e3f67ad0b5c92217c32a89876e921a06e
claire froelich <claire.froelich@gmail.com>
2020-09-22 09:06:57 -0700

    Add unit tests for createProject()

--- test/src/tests/projectService.test.ts
+++ test/src/tests/projectService.test.ts
@@ -19,7 +19,7 @@ describe("ProjectService", () => {
         context.projectDao.clearMockReturns();
       });
     });
-    it("returns a projects on success", async () => {
+    it("returns a project on success", async () => {
       await testManager
         .configureContext((context) => {
           context.projectDao.mockReturn({ data: fakeProject });
@@ -46,14 +46,55 @@ describe("ProjectService", () => {
       expect(finalState.errors[0].message).toBe(SERVER_ERR_MESSAGE);
       expect(finalState.toasts[0].type).toBe("DANGER");
     });
-    // it("returns an empty array if no meets are in db", async () => {
-    //   const tm = testManager.addMeets([]);
-    //
-    //   await tm.execute((context) => {
-    //     return context.meetService.fetchMeets().then((result) => {
-    //       expect(result.length).toBe(0);
-    //     });
-    //   });
-    // });
+  });
+  describe("createProject()", () => {
+    beforeEach(() => {
+      testManager = TestManager.build();
+    });
+
+    afterEach(() => {
+      // Just to be safe!
+      testManager.configureContext((context) => {
+        context.projectDao.clearMockReturns();
+      });
+    });
+
+    const newProjectParams: CreateProjectParams = {
+      title: fakeProject.title,
+      meetId: fakeProject.meet.id,
+      sourceCodeUrl: fakeProject.sourceCodeUrl,
+      liveUrl: fakeProject.liveUrl,
+      cloudinaryPublicIds: fakeProject.mediaAssets.map((ma) => ma.cloudinaryPublicId),
+    };
+
+    it("returns new project and throws successs toast on success", async () => {
+      await testManager
+        .configureContext((context) => {
+          context.projectDao.mockReturn({ data: fakeProject });
+        })
+        .execute((context) => {
+          return context.projectService.createProject(newProjectParams).then((result) => {
+            expect(result).toMatchObject(fakeProject);
+          });
+        });
+      const finalStoreState = testManager.store.getState();
+      expect(finalStoreState.toasts[0].type).toBe("SUCCESS");
+    });
+    it("logs error and throws toast on failure", async () => {
+      const SERVER_ERR_MESSAGE = "Test msg";
+      const FAKE_ERROR = { data: null, errors: [{ message: SERVER_ERR_MESSAGE, extensions: { code: "TEST" } }] };
+      await testManager
+        .configureContext((context) => {
+          context.projectDao.mockReturn(FAKE_ERROR);
+        })
+        .execute((context) => {
+          return context.projectService.createProject(newProjectParams).then((result) => {
+            expect(result).toBe(undefined);
+          });
+        });
+      const finalState = testManager.store.getState();
+      expect(finalState.errors[0].message).toBe(SERVER_ERR_MESSAGE);
+      expect(finalState.toasts[0].type).toBe("DANGER");
+    });
   });
 });
