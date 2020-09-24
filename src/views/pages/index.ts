e69c86f650697516ae09cb540be74b2649dc0b47
claire froelich <claire.froelich@gmail.com>
2020-09-24 11:40:57 -0700

    Add tos and privacy policy pages

--- src/views/pages/index.ts
+++ src/views/pages/index.ts
@@ -1,6 +1,8 @@
 export { default as Home } from "./Home";
 export { default as Hackathons } from "./Hackathons";
 export { default as Community } from "./Community";
+export { default as TermsOfService } from "./Legal/TermsOfService";
+export { default as PrivacyPolicy } from "./Legal/PrivacyPolicy";
 export { default as Meets } from "./Meets";
 export { default as Meet } from "./templates/Meet";
 export { default as Project } from "./templates/Project";
