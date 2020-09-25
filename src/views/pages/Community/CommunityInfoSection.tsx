d06c2384f0cb8e8a6ab9517d95b328a79b5686d0
claire froelich <claire.froelich@gmail.com>
2020-09-24 18:18:54 -0700

    Add discord link and tweak layout

--- src/views/pages/Community/CommunityInfoSection.tsx
+++ src/views/pages/Community/CommunityInfoSection.tsx
@@ -17,12 +17,12 @@ export const InfoSection: FC<Props> = ({ section }) => {
     <section className="grid md:grid-cols-5">
       <div className=" col-span-2 bg-mb-blue-200 pr-12 pl-2 md:mr-20 mt-16 mb-12 md:mt-24 md:mb-40 rounded-r-mb-md shadow-mb-drop">
         <img
-          className="block w-5/6 ml-auto relative top-mb-3"
+          className="block w-5/6 ml-auto  relative top-mb-3"
           src={shapesImg}
           alt="a triangle, a circle and a square intertwining"
         />
       </div>
-      <div className="col-span-3 px-10 md:pl-0 md:pr-24 place-self-end mb-20 text-center md:text-right">
+      <div className="col-span-3 px-10 pt-20 md:pl-0 md:pr-24 place-self-end mb-20 text-center md:text-right">
         <h3 className=" text-2xl tracking-wide font-medium">
           A<span className="font-semibold text-mb-green-100"> {tagline}</span> community
         </h3>
