66f07c9e8ea63bcf03580f78f6eb18ee47bfdb76
theFl00f <celeste.ellerby@gmail.com>
2020-10-01 14:42:58 -0400

    separated PastMeetCard from MeetCard, center text, flip text and img back to original order, expanded clickable area, altered grid row

--- src/views/components/MeetCard.tsx
+++ src/views/components/MeetCard.tsx
@@ -9,19 +9,17 @@ const d = new DateUtility();
 type MeetProps = {
   meet: Meet;
   user?: User;
-  type?: "current" | "past";
   onDelete: () => Promise<void>;
 };
 
-export const MeetCard: FC<MeetProps> = ({ meet, user, type = "current", onDelete }) => {
+export const MeetCard: FC<MeetProps> = ({ meet, user, onDelete }) => {
   const { id, title, description, startTime, endTime, coverImageUrl, region } = meet;
 
   const startTimeStr = d.wcToClientStr(startTime, region);
   const endTimeStr = d.wcToClientStr(endTime, region);
-  const pastEndTimeStr = endTimeStr.slice(0, 17);
   // const durationInHours = d.getDurationInHours(startTime, endTime);
   // const durationString = d.getDurationStringFromHours(durationInHours);
-  return type == "current" ? (
+  return (
     <div className="shadow-md bg-white md:pr-8 flex-col md:flex md:flex-row w-11/12 mx-auto rounded-lg lg:p-4 overflow-hidden">
       <div className="h-64 md:w-1/3 md:h-56 overflow-hidden inline-flex justify-center items-center my-auto mx-auto">
         <img className="object-cover" src={coverImageUrl} alt={`${title} event banner`}></img>
@@ -45,28 +43,6 @@ export const MeetCard: FC<MeetProps> = ({ meet, user, type = "current", onDelete
             )}
           </div>
         </section>
-      </div>
-    </div>
-  ) : (
-    <div className="shadow-md bg-white overflow-hidden rounded-mb-sm grid grid-rows-2 p-2">
-      <section className="flex flex-col items-center justify-between h-full p-2">
-        <div className="w-full">
-          <p className="text-xs truncate text-center">
-            {pastEndTimeStr} - <span className="uppercase">completed</span>
-          </p>
-          <h2 className="text-lg text-center font-medium md:break-all lg:break-normal">{title}</h2>
-          <p className="text-sm">{description}</p>
-        </div>
-      </section>
-      <Link to={`/meets/${id}`} className="h-56 overflow-hidden inline-grid place-items-center">
-        <img
-          className="object-cover w-auto min-h-full transition duration-500 ease-in-out transform scale-125 hover:scale-150"
-          src={coverImageUrl}
-          alt={`${title} event banner`}
-        ></img>
-      </Link>
-      <div className="flex justify-center">
-        {user?.isAdmin && <AdminMeetDeleteModal buttonText="Delete" meet={meet} onDelete={onDelete} className="mt-2" />}
       </div>
     </div>
   );
---
+++ src/views/components/PastMeetCard.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+import { DateUtility } from "../../utils/DateUtility";
+import { Link } from "react-router-dom";
+import AdminMeetDeleteModal from "./wrappers/Modal/walas/AdminMeetDeleteModal";
+
+const d = new DateUtility();
+
+type MeetProps = {
+  meet: Meet;
+  user?: User;
+  onDelete: () => Promise<void>;
+};
+export const PastMeetCard: FC<MeetProps> = ({ meet, user, onDelete }) => {
+  const { id, title, description, endTime, coverImageUrl, region } = meet;
+
+  const endTimeStr = d.wcToClientStr(endTime, region);
+  const pastEndTimeStr = endTimeStr.slice(0, 17);
+  let descriptionStr = description.slice(0, 161);
+  description.length > 161 ? (descriptionStr = descriptionStr + "...") : descriptionStr;
+
+  return (
+    <div className="shadow-md bg-white overflow-hidden rounded-mb-sm flex flex-col">
+      <Link to={`/meets/${id}`} className="grid grid-rows-3 text-black flex-grow">
+        <div className="h-56 overflow-hidden inline-grid place-items-center row-span-2">
+          <img
+            className="object-cover w-auto min-h-full transition duration-500 ease-in-out transform scale-125 hover:scale-150"
+            src={coverImageUrl}
+            alt={`${title} event banner`}
+          ></img>
+        </div>
+        <section className="flex flex-col items-center h-full py-2 px-4 lg:px-6 w-full">
+          <p className="text-xs truncate text-center">
+            {pastEndTimeStr} - <span className="uppercase">completed</span>
+          </p>
+          <h2 className="text-lg text-center font-medium md:break-all lg:break-normal">{title}</h2>
+          <p className="text-sm justify-self-end my-auto text-justify">{descriptionStr}</p>
+        </section>
+      </Link>
+      <div className="flex justify-center">
+        {user?.isAdmin && <AdminMeetDeleteModal buttonText="Delete" meet={meet} onDelete={onDelete} className="my-2" />}
+      </div>
+    </div>
+  );
+};
