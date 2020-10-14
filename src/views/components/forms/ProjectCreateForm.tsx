d63ebb4fc2d186abe2aef6641af0af2ab20996d6
Claire Froelich <claire.froelich@gmail.com>
2020-10-14 09:43:37 -0700

    Remove unused code

--- src/views/components/forms/ProjectCreateForm.tsx
+++ src/views/components/forms/ProjectCreateForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC, useState } from "react";
 import { yupResolver } from "@hookform/resolvers";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
