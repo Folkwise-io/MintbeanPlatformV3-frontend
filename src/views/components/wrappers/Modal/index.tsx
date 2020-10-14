2adbdce60cdabb115746b76ba01fb55eeeaba8a8
Claire Froelich <claire.froelich@gmail.com>
2020-10-14 09:48:45 -0700

    Fix type error in styling

--- src/views/components/wrappers/Modal/index.tsx
+++ src/views/components/wrappers/Modal/index.tsx
@@ -76,7 +76,7 @@ export const Modal: FC<ModalProps> = ({
     : [];
 
   // style for centering modal in middle of screen if isDetached prop = true
-  const detachedStyles = { left: "50%", top: "50%", transform: "translate(-50%, -50%)", zIndex: "999" };
+  const detachedStyles = { left: "50%", top: "50%", transform: "translate(-50%, -50%)", zIndex: 999 };
   return (
     <>
       <div>
