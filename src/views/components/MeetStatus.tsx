6638302a1ee9e27572584ab5bde7379f5b87933e
theFl00f <celeste.ellerby@gmail.com>
2020-10-15 16:57:03 -0400

    added wrap when screensize is small on meetstatus

--- src/views/components/MeetStatus.tsx
+++ src/views/components/MeetStatus.tsx
@@ -77,7 +77,7 @@ export const MeetStatus: FC<Props> = ({ meet, user, isBordered = false }) => {
   const registeredDefinition = registeredBuilder(isBordered);
 
   return (
-    <>
+    <span className="flex flex-wrap justify-center md:flex-no-wrap">
       {definitionType === "REGISTERED" && definitionBuilder === defStep1.OPEN && (
         <span className={`${registeredDefinition.classes + " " + registeredDefinition.borderClass} mr-2`}>
           <FontAwesomeIcon icon={registeredDefinition.icons} className="mr-1 my-auto" />
@@ -88,6 +88,6 @@ export const MeetStatus: FC<Props> = ({ meet, user, isBordered = false }) => {
         <FontAwesomeIcon icon={definition.icons} className="mr-1 my-auto" />
         {definition.spanText}
       </span>
-    </>
+    </span>
   );
 };
