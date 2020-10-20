57994cd345ef54b53d2f9bc935836e761857f131
theFl00f <celeste.ellerby@gmail.com>
2020-10-20 17:34:15 -0400

    un-nuked login/logout

--- src/views/components/Navbar.tsx
+++ src/views/components/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar: FC<StateMapping & DispatchMapping> = ({ user, logout }) => {
         <TinyFabNav />
       </div>
       <div className="flex flex-col md:flex-row md:items-center justify-between md:py-2">
-        <section className="h-full sm:w-56 mx-auto md:mx-0">
+        <section className="h-full sm:w-56 mx-auto md:mx-0 pb-4 md:pb-0">
           <Link
             to="/"
             className="transition duration-500 ease-in-out text-black hover:text-mb-blue-100 focus:text-mb-blue-100 grid place-items-center md:place-items-start"
@@ -55,9 +55,9 @@ const Navbar: FC<StateMapping & DispatchMapping> = ({ user, logout }) => {
             <img src={logo} alt="Mintbean logo" className="" style={{ maxHeight: "50px" }} />
           </Link>
         </section>
-        <section className="hidden md:block">
+        <section>
           <div className="flex flex-col md:flex-row items-center">
-            <div>
+            <div className="hidden md:block">
               <Link
                 to="/community"
                 className="transition duration-500 ease-in-out mx-2 text-black hover:text-mb-blue-100 focus:text-mb-blue-100"
