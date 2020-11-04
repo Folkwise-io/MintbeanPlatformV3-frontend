258df598429bc6d64fcdd092aa9dbe866cb79acd
theFl00f <celeste.ellerby@gmail.com>
2020-11-04 10:47:35 -0500

    input setup

--- src/views/pages/Admin/BadgeLookup.tsx
+++ src/views/pages/Admin/BadgeLookup.tsx
@@ -1,6 +1,7 @@
 import { IconLookup, IconDefinition, findIconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { FC, useCallback, useEffect, useState } from "react";
+import { default as Select } from "react-select";
 import { ConnectContextProps, connectContext } from "../../../context/connectContext";
 import { Badge } from "../../../types/badge";
 
@@ -8,6 +9,9 @@ const BadgeLookup: FC<ConnectContextProps> = ({ context }) => {
   const [badges, setBadges] = useState<Badge[]>([]);
   const [badge, setBadge] = useState<Badge>();
   const [, setLoading] = useState<boolean>(false);
+  const badgeSearchOptions = badges.map(({ badgeId, alias }) => ({ value: badgeId, label: alias }));
+  // const [selectedValue, setSelectedValue] = useState(badgeSearchOptions[0].value);
+
   const fetchBadgesData = useCallback(async () => {
     if (!context) {
       console.error(new Error("No context passed to component, but it was expected"));
@@ -41,43 +45,58 @@ const BadgeLookup: FC<ConnectContextProps> = ({ context }) => {
     fetchBadgesData();
     fetchBadgeData("00000000-0000-0000-0000-000000000000");
   }, [context, fetchBadgesData, fetchBadgeData]);
+
+  // const handleChange = (option: any) => {
+  //   setSelectedValue(option.value);
+  //   iconLookup = { prefix: "fas", iconName: selectedValue };
+  //   iconDefinition = findIconDefinition(iconLookup);
+  // };
+
+  console.log(badgeSearchOptions[0]);
   return (
-    <ul className="grid grid-cols-3 place-items-center">
-      {badge && <div>{badge.alias}</div>}
-      {badges.map((badge, index) => {
-        const {
-          badgeId,
-          alias,
-          badgeShape,
-          faIcon,
-          backgroundHex,
-          iconHex,
-          title,
-          description,
-          weight,
-          createdAt,
-        } = badge;
-        const iconLookup: IconLookup = { prefix: "fas", iconName: faIcon };
-        const iconDefinition: IconDefinition = findIconDefinition(iconLookup);
-        return (
-          <li key={index} className="flex flex-col items-center">
-            <div
-              className="mb-flex-centered"
-              style={{
-                backgroundColor: `#${backgroundHex}`,
-                height: 50,
-                width: 50,
-                fontSize: 25,
-                borderRadius: "50%",
-              }}
-            >
-              <FontAwesomeIcon icon={iconDefinition} style={{ color: `#${iconHex}` }} />
-            </div>
-            <p>{alias}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <Select
+        options={badgeSearchOptions}
+        // value={badgeSearchOptions.filter((obj) => obj.value === selectedValue)}
+        // onChange={(option) => handleChange(option)}
+      ></Select>
+      <ul className="grid grid-cols-3 place-items-center">
+        {badge && <div>{badge.alias}</div>}
+        {badges.map((badge, index) => {
+          const {
+            badgeId,
+            alias,
+            badgeShape,
+            faIcon,
+            backgroundHex,
+            iconHex,
+            title,
+            description,
+            weight,
+            createdAt,
+          } = badge;
+          const iconLookup: IconLookup = { prefix: "fas", iconName: faIcon };
+          const iconDefinition: IconDefinition = findIconDefinition(iconLookup);
+          return (
+            <li key={index} className="flex flex-col items-center">
+              <div
+                className="mb-flex-centered"
+                style={{
+                  backgroundColor: `#${backgroundHex}`,
+                  height: 50,
+                  width: 50,
+                  fontSize: 25,
+                  borderRadius: "50%",
+                }}
+              >
+                <FontAwesomeIcon icon={iconDefinition} style={{ color: `#${iconHex}` }} />
+              </div>
+              <p>{alias}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
