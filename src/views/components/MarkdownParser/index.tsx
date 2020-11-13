4ea44da22e917805e369855b1e54cfbeae17223d
Claire Froelich <claire.froelich@gmail.com>
2020-11-13 12:32:57 -0800

    Add dynamic syntax highlighting to markdown

--- src/views/components/MarkdownParser/index.tsx
+++ src/views/components/MarkdownParser/index.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from "react";
 import Markdown from "react-markdown";
+import gfm from "remark-gfm";
+import CodeBlock from "./CodeBlock";
+
 import "./markdown.css";
 
 type Props = {
@@ -9,5 +12,12 @@ type Props = {
 
 /* Returns markdown as html in a div with bas class name "markdown". Styled in index.css */
 export const MarkdownParser: FC<Props> = ({ source, className }) => {
-  return <Markdown source={source} className={`markdown ${className ? className : ""}`} />;
+  return (
+    <Markdown
+      source={source}
+      plugins={[gfm]}
+      renderers={{ code: CodeBlock }}
+      className={`markdown ${className ? className : ""}`}
+    />
+  );
 };
