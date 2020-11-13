5b739cc8207c51441b814f5fa311b3c29f9a4795
Claire Froelich <claire.froelich@gmail.com>
2020-11-13 13:26:40 -0800

    Render all markdown anchors as ExternalLink components

--- src/views/components/MarkdownParser/index.tsx
+++ src/views/components/MarkdownParser/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Markdown from "react-markdown";
 import gfm from "remark-gfm";
+import { ExternalLink } from "../ExternalLink";
 import CodeBlock from "./CodeBlock";
 
 import "./markdown.css";
@@ -12,11 +13,20 @@ type Props = {
 
 /* Returns markdown as html in a div with bas class name "markdown". Styled in index.css */
 export const MarkdownParser: FC<Props> = ({ source, className }) => {
+  // render all anchor tags from markdown as external links
+  const link = (props: React.HTMLProps<HTMLAnchorElement>): React.ReactElement<typeof ExternalLink> => {
+    return <ExternalLink href={props.href || "#"} {...props} />;
+  };
+
+  // The renderers object is for overriding the default renderers with desired React component.
+  // Refer to deafult renderers: https://github.com/remarkjs/react-markdown/blob/main/src/renderers.js
+  const renderers = { code: CodeBlock, link };
+
   return (
     <Markdown
       source={source}
       plugins={[gfm]}
-      renderers={{ code: CodeBlock }}
+      renderers={renderers}
       className={`markdown ${className ? className : ""}`}
     />
   );
