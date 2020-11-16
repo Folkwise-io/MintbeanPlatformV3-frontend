e8506a0cae38a5392898e01111eb1dcfda866c64
Claire Froelich <claire.froelich@gmail.com>
2020-11-16 15:49:04 -0800

    Remove lenght limit on kanban card body edit schema

--- src/views/components/forms/KanbanCanonCardEditForm.tsx
+++ src/views/components/forms/KanbanCanonCardEditForm.tsx
@@ -7,7 +7,7 @@ import { MarkdownEditor } from "../MarkdownEditor";
 /* TODO: CENTRALIZE & SYNC YUP SCHEMAS IN BACKEND*/
 const editKanbanCanonInputSchema = yup.object().shape({
   title: yup.string().min(2, "Too Short!").max(64, "Too Long!"),
-  body: yup.string().min(3, "Too Short!").max(150, "Too Long!"),
+  body: yup.string().min(3, "Too Short!"),
 });
 
 interface Props {
