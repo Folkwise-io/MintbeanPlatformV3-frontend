c048c7fdba4e5da36515a83d9d31c598eb29e1e3
Celeste Ellerby <53948525+theFl00f@users.noreply.github.com>
2020-12-08 11:47:49 -0500

    moved broken pages into {updated} components and reverted render back to previous meets page and pastMeetCard component

--- src/views/components/MeetCards/PastMeetCard.tsx
+++ src/views/components/MeetCards/PastMeetCard.tsx
@@ -6,15 +6,12 @@ import { Meet } from "../../../types/meet";
 import { wcToClientStr } from "../../../utils/DateUtility";
 import { AdminMeetDeleteModal } from "../wrappers/Modal/walas/AdminMeetDeleteModal";
 import { MeetStatus } from "./MeetStatus";
-import { MeetType } from "./MeetType";
 
 type MeetProps = {
   meet: Meet;
   user?: User;
   onDelete: () => Promise<void>;
 };
-
-// TODO: this will be renamed MeetCard and MeetCard will be deleted
 export const PastMeetCard: FC<MeetProps> = ({ meet, user, onDelete }) => {
   const { id, title, description, endTime, coverImageUrl, region } = meet;
 
@@ -25,37 +22,32 @@ export const PastMeetCard: FC<MeetProps> = ({ meet, user, onDelete }) => {
 
   return (
     <div className="shadow-md bg-white overflow-hidden rounded-mb-sm flex flex-col border-solid border-white border-2">
-      <Link to={`/meets/${id}`} className="grid grid-rows-5 h-72 text-black flex-grow">
-        <div className="h-full row-span-3 overflow-hidden inline-grid place-items-center relative">
+      <Link to={`/meets/${id}`} className="grid grid-rows-2 lg:grid-rows-3 text-black flex-grow">
+        <div className="h-full max-h-72 overflow-hidden inline-grid place-items-center lg:row-span-2">
           <img
             className="object-contain bg-black w-auto min-h-full mb-transition transform scale-100 hover:scale-125"
             src={coverImageUrl}
             alt={`${title} event banner`}
-          />
-          <div className="absolute top-mb-1 right-mb-1">
-            <MeetStatus user={user} meet={meet} isBordered />
-          </div>
+          ></img>
         </div>
-        <section className="flex flex-col items-center h-full w-full row-span-2">
-          <div className="pt-2 pb-1 px-4 lg:px-6 flex flex-col items-center h-full w-full">
-            <div className="mb-flex-centered w-full">
-              <p className="font-semibold text-xs truncate text-center text-mb-gray-200">
-                <FontAwesomeIcon icon={faClock} className="mx-1" />
-                <span className="font-normal">{pastEndTimeStr}</span>
-              </p>
-            </div>
-            <h2 className="text-lg text-center font-medium">{title}</h2>
-            <div className="flex-grow flex items-end">
-              <MeetType meetType={meet.meetType} />
+        <section className="flex flex-col items-center h-full py-2 px-4 lg:px-6 w-full">
+          <div className="flex flex-col xs:flex-row xs:justify-between w-full items-center xs:mb-4">
+            <p className="font-semibold text-xs truncate text-center text-mb-gray-200">
+              Ended
+              <FontAwesomeIcon icon={faClock} className="mx-1" />
+              <span className="font-normal">{pastEndTimeStr}</span>
+            </p>
+            <div className="flex">
+              <MeetStatus user={user} meet={meet} />
             </div>
           </div>
+          <h2 className="text-lg text-center font-medium md:break-all lg:break-normal">{title}</h2>
+          <p className="text-sm justify-self-end my-auto text-center">{descriptionStr}</p>
         </section>
       </Link>
-      {user?.isAdmin && (
-        <div className="flex justify-center my-2">
-          <AdminMeetDeleteModal buttonText="Delete" meet={meet} onDelete={onDelete} />
-        </div>
-      )}
+      <div className="flex justify-center my-2">
+        {user?.isAdmin && <AdminMeetDeleteModal buttonText="Delete" meet={meet} onDelete={onDelete} />}
+      </div>
     </div>
   );
 };
--- src/views/pages/Meets.tsx
+++ src/views/pages/Meets.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState, useEffect, useCallback, useContext, ChangeEvent } from "react";
+import React, { FC, useState, useEffect, useCallback, useContext } from "react";
+import { MeetCard } from "../components/MeetCards/MeetCard";
 import { AdminMeetCreateModal } from "../components/wrappers/Modal/walas/AdminMeetCreateModal";
 import { connect } from "react-redux";
 import { BgBlock } from "../components/BgBlock";
@@ -7,11 +8,7 @@ import { isPast } from "../../utils/DateUtility";
 import { PastMeetCard } from "../components/MeetCards/PastMeetCard";
 import { MbContext } from "../../context/MbContext";
 import { Context } from "../../context/contextBuilder";
-import { Select } from "../components/blocks/Form/Select";
-import { dateFilterOptions, meetTypeFilterOptions } from "../components/forms/constants";
-import { Input } from "../components/blocks/Form/Input";
 import { Meet } from "../../types/meet";
-import { MeetTypeEnum } from "../../types/enum";
 
 interface StateMapping {
   user: UserState;
@@ -23,9 +20,6 @@ const stp = (state: StoreState) => ({
 const Meets: FC<StateMapping> = ({ user }) => {
   const context = useContext<Context>(MbContext);
   const [meets, setMeets] = useState<Meet[]>([]);
-  const [meetType, setMeetType] = useState<MeetTypeEnum | "all">("all");
-  const [searchInput, setSearchInput] = useState<string>("");
-  const [dateFilter, setDateFilter] = useState<MeetDate>("upcoming");
   const [loading, setLoading] = useState<boolean>(false);
 
   const fetchMeets = useCallback(async () => {
@@ -41,6 +35,13 @@ const Meets: FC<StateMapping> = ({ user }) => {
     fetchMeets();
   }, [context, fetchMeets]);
 
+  // reverse-chronological sort
+  const renderPastMeets = () =>
+    meets
+      .filter((m: Meet) => isPast(m.endTime, m.region))
+      .sort((a, b) => new Date(b.startTime).getTime() - new Date(a.startTime).getTime())
+      .map((meet) => <PastMeetCard meet={meet} key={meet.id} user={user.data} onDelete={fetchMeets} />);
+
   const renderAdminMeetCreateModal = () =>
     user.data?.isAdmin && (
       <div className="flex justify-center">
@@ -51,138 +52,60 @@ const Meets: FC<StateMapping> = ({ user }) => {
       </div>
     );
 
-  const renderMeets = () => {
+  const renderUpcomingMeets = () => {
     if (loading) {
       return <p className="text-white">Loading...</p>;
     }
 
-    //filteredMeets default to all
-    let filteredMeets = meets;
-
-    //if not all but has datefilter
-    if (dateFilter && dateFilter !== "all") {
-      //&& is past
-      if (dateFilter === "past") {
-        filteredMeets = meets.filter((m: Meet) => isPast(m.endTime, m.region));
-      } else if (dateFilter === "upcoming") {
-        //or upcoming
-        filteredMeets = meets.filter((m: Meet) => !isPast(m.endTime, m.region));
-      }
-    }
-
-    //if there's a selected meetType, filter meets by type
-    if (meetType !== "all") {
-      filteredMeets = filteredMeets.filter((m: Meet) => m.meetType === meetType);
-    }
-
-    //if there's a search input, search the description and title for that string
-    if (searchInput) {
-      filteredMeets = filteredMeets.filter((m: Meet) =>
-        `${m.description} ${m.title}`.toLowerCase().includes(searchInput.toLowerCase()),
-      );
-    }
-
-    //sort by date
-    filteredMeets = filteredMeets.sort((a, b) => {
-      const dateA = new Date(a.startTime).getTime();
-      const dateB = new Date(b.startTime).getTime();
-      if (dateA === dateB) return 0;
-      if (dateFilter === "upcoming") {
-        //if upcoming, ascending date
+    // chronological sort
+    const upcomingMeets = meets
+      .filter((m: Meet) => !isPast(m.endTime, m.region))
+      .sort((a, b) => {
+        const dateA = new Date(a.startTime).getTime();
+        const dateB = new Date(b.startTime).getTime();
+        if (dateA === dateB) return 0;
         return dateA - dateB;
-      } else {
-        //else, descending date
-        return dateB - dateA;
-      }
-    });
-    //save mapped meets
-    const mappedMeets = filteredMeets.map((meet) => (
-      <PastMeetCard meet={meet} key={meet.id} user={user.data} onDelete={fetchMeets} />
-    ));
-    //if there are meets in the filtered array, map and render
-    if (filteredMeets.length) {
-      return mappedMeets;
-    } else if (meets) {
-      //if there are no meets in the filtered array but meets exist, return error message
-      let errorMessage = `No results matched your filters, please try again.`;
-      if (searchInput) {
-        errorMessage = `No ${dateFilter} ${
-          meetType !== "all" ? meetType : "meet"
-        }s found matching the term "${searchInput}"`;
-      }
-      return <p className="text-white text-lg">{errorMessage}</p>;
+      })
+      .map((meet) => <MeetCard meet={meet} key={meet.id} user={user.data} onDelete={fetchMeets} />);
+
+    if (upcomingMeets.length) {
+      return <div className="space-y-4">{upcomingMeets}</div>;
     }
-    //if there are no meets at all, return error message - this should not be reached.
-    return <p className="text-white text-lg">No meets at the moment... Stay tuned!</p>;
-  };
 
-  const handleMeetTypeChange = (e: ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
-    setMeetType(target.value as MeetTypeEnum);
+    return <p className="text-white text-lg">No upcoming meets at the moment... Stay tuned!</p>;
   };
-
-  const handleSearchInputChange = (e: ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
-    setSearchInput(target.value);
-  };
-
-  const handleDateFilterChange = (e: ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
-    setDateFilter(target.value as MeetDate);
-  };
-
   return (
     <div className="mb-8">
-      <main>
+      <BgBlock type="gradStripeEvents">
         <BgBlock type="blackStripeEvents">
-          <BgBlock type="black">
-            <header className="md:pt-8 md:pb-6 flex flex-col items-center">
-              <FocusCard
-                type="eventsTitle"
-                card={{
-                  title: "Meets",
-                  description: "Come hack with us!",
-                }}
-              />
-              {renderAdminMeetCreateModal()}
-            </header>
-          </BgBlock>
-          <section className="max-w-7xl mx-auto flex flex-col items-center pt-12 pb-24 md:pb-20 px-6 md:px-24">
-            <fieldset className="bg-white w-11/12 flex items-center flex-wrap rounded-mb-xs justify-evenly mb-4">
-              <div>
-                <Input
-                  type="text"
-                  label="Search:"
-                  name="searchMeets"
-                  className="m-2"
-                  onChange={handleSearchInputChange}
-                />
-              </div>
-              <div>
-                <Select
-                  name="meetTypeFilter"
-                  label="Filter by meet type:"
-                  options={meetTypeFilterOptions}
-                  onChange={handleMeetTypeChange}
-                  className="m-2"
-                />
-              </div>
-              <div>
-                <Select
-                  name="dateFilter"
-                  label="filter by date:"
-                  options={dateFilterOptions}
-                  onChange={handleDateFilterChange}
-                  className="m-2"
-                />
-              </div>
-            </fieldset>
-            <div className="grid grid-cols-1 px-0 sm:px-12 md:px-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 row-auto gap-6">
-              {renderMeets()}
-            </div>
-          </section>
+          <header className="md:pt-8 md:pb-6 flex flex-col items-center">
+            <FocusCard
+              type="eventsTitle"
+              card={{
+                title: "Meets",
+                description: "Come hack with us!",
+              }}
+            />
+            {renderAdminMeetCreateModal()}
+          </header>
         </BgBlock>
-      </main>
+        <main>
+          <BgBlock type="black">
+            <BgBlock>
+              <section className="rounded-xl mb-16 flex flex-col items-center w-full py-8 bg-black max-w-6xl shadow-mb-drop-center">
+                <h2 className="text-4xl text-white mb-4 font-semibold text-center">Upcoming meets</h2>
+                {renderUpcomingMeets()}
+              </section>
+            </BgBlock>
+            <section className="max-w-6xl mx-auto flex flex-col items-center pt-12 pb-24 md:pb-20 px-6 md:px-24">
+              <h2 className="text-white text-4xl mb-4 text-center">Past meets</h2>
+              <div className="grid grid-cols-1 px-0 sm:px-12 md:px-0 md:grid-cols-2 row-auto gap-6">
+                {renderPastMeets()}
+              </div>
+            </section>
+          </BgBlock>
+        </main>
+      </BgBlock>
     </div>
   );
 };
