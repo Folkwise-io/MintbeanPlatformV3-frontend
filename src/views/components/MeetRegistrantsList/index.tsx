969359d737c47fddbce5a73135308dd3a1c7becd
Dan Reardon <dreardon1021@gmail.com>
2020-12-22 12:47:48 -0500

    Add Component visibility open and close

--- src/views/components/MeetRegistrantsList/index.tsx
+++ src/views/components/MeetRegistrantsList/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from "react";
 import greyProfilePic from "../../../assets/images/logos/grey-profile-pic.svg";
+import "./meetRegistrantsList.css"
 
 interface Props {
   meetRegistrants: RegistrantsForMeet[];
@@ -10,26 +11,58 @@ export const MeetRegistrantsList: FC<Props> = ({ meetRegistrants }) => {
 
   let containerView;
 
+  const switchVisibility = () => {
+    if (visible) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  };
+
   if (visible) {
     containerView = (
-      <div className="bg-white max-w-sm">
-        <div>
-          <p>{meetRegistrants.length} Attendees</p>
-          <p className="text-">Hide All</p>
+      <div className="bg-mb-green-200 min-w-lg min-h-screen flex justify-center items-center">
+        <div className="bg-black min-w-lg rounded-md max-h-32 p-4">
+          <div className="flex justify-between">
+            <p className="text-white">{meetRegistrants.length} Attendees</p>
+            <p onClick={switchVisibility} className="text-mb-green-200 cursor-pointer hover:underline">
+              Hide All
+            </p>
+          </div>
+          <div className="overflow-y-scroll">
+          {meetRegistrants.map((r: RegistrantsForMeet) => {
+            // This will be its own component
+            return (
+              <div className="flex align-center mt-6" key={r.id}>
+                <img src={greyProfilePic} />
+                <div className="ml-3">
+                  <p className="text-mb-blue-300 font-bold">
+                    {r.firstName} {r.lastName}
+                  </p>
+                  <p className="text-white font-light">Member since 2020</p>
+                </div>
+              </div>
+            );
+            //return statement end above to commment is MeetListUser component
+          })}
+          </div>
         </div>
-
-        {meetRegistrants.map((r: RegistrantsForMeet) => {
-          return (
-            <div key={r.id}>
-              <img src={greyProfilePic} />
-              <p className="text-white">{r.id}</p>
-            </div>
-          );
-        })}
       </div>
     );
   } else {
-    containerView = <h1>asdasd</h1>;
+    containerView = (
+      <div className="bg-mb-green-200 min-w-lg min-h-screen flex justify-center items-center">
+        <div className="bg-black min-w-lg rounded-md max-h-32 p-4">
+          <div className="flex justify-between">
+            <p className="text-white">{meetRegistrants.length} Attendees</p>
+            <p onClick={switchVisibility} className="text-mb-green-200 cursor-pointer hover:underline">
+              Show All
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    );
   }
 
   return containerView;
