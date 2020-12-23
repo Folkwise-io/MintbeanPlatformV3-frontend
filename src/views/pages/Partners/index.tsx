f46d5ce51b324b12f2f34bd935febe520852194a
Claire Froelich <claire.froelich@gmail.com>
2020-12-22 16:57:12 -0800

    Add partner logo section and semantic sections

--- src/views/pages/Partners/index.tsx
+++ src/views/pages/Partners/index.tsx
@@ -11,6 +11,8 @@ import svgMintyBaconLgGradient from "../../../assets/images/backgrounds/minty-ba
 import svgWhiteCurvedYTop from "../../../assets/images/backgrounds/white-curved-y-top.svg";
 import svgWhiteCurvedYBottom from "../../../assets/images/backgrounds/white-curved-y-bottom.svg";
 import svgPartnerPillars from "../../../assets/images/figures/partner-pillars.svg";
+// TODO: remove once assets arrive
+import svgTEMPLogo from "../../../assets/images/tmp/LOGO.svg";
 
 const topStats = [
   {
@@ -58,72 +60,88 @@ const pillars = [
   },
 ];
 
+// TODO: replace with real logos
+const partnerLogos = [
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+  { logo: svgTEMPLogo, name: "Foo" },
+];
+
 const Partners: FC = () => {
   return (
     <div className="bg-mb-blue-250 min-h-screen pb-24">
       {/* Header start */}
-      <div className="bg-mb-blue-500">
-        <Container>
-          <div className="pt-24 pb-10">
-            <H1 className="text-center mb-8">Not your typical hackathon.</H1>
-            <div className="flex flex-col items-start sm:flex-row flex-wrap w-full md:justify-center">
-              {topStats.map(({ stat, label }, i) => (
-                <Stat key={i} statColor="white" stat={stat} label={label} className="mx-8 my-2" />
-              ))}
+      <section>
+        <div className="bg-mb-blue-500 w-full">
+          <Container>
+            <div className="pt-24 pb-10">
+              <H1 className="text-center mb-8">Not your typical hackathon.</H1>
+              <div className="flex flex-col items-start sm:flex-row flex-wrap w-full md:justify-center">
+                {topStats.map(({ stat, label }, i) => (
+                  <Stat key={i} statColor="white" stat={stat} label={label} className="mx-8 my-2" />
+                ))}
+              </div>
             </div>
-          </div>
-        </Container>
-      </div>
-
-      {/* Stats 1 start */}
-      <div className="w-full relative">
-        <img src={svgMintyBaconLgGradient} alt="" className="w-full" />
-      </div>
-
-      {/* Text section start */}
+          </Container>
+        </div>
+        <div className="w-full relative">
+          <img src={svgMintyBaconLgGradient} alt="" className="w-full" />
+        </div>
+      </section>
+      {/* "Why JavaScript developers loves us" start */}
       <Container className="mt-12">
-        <H2 className="text-center">Why JavaScript developers loves us.</H2>
-        <p>
-          Our vibrant and rapidly-growing hackathons and events attract developers from diverse backgrounds who are
-          interested in cutting-edge JavaScript technologies like React, Typescript, GraphQL, ExpressJS, MongoDB and
-          more.
-        </p>
-        <p>
-          Engineers come to our hackathons, workshops and events to connect with each other & learn new technology. Our
-          community is known to expose its members to the newest ground-breaking frameworks, libraries, APIs and SDKs –
-          often before they achieve breakout popularity.
-        </p>
+        <section>
+          <H2 className="text-center">Why JavaScript developers loves us.</H2>
+          <p>
+            Our vibrant and rapidly-growing hackathons and events attract developers from diverse backgrounds who are
+            interested in cutting-edge JavaScript technologies like React, Typescript, GraphQL, ExpressJS, MongoDB and
+            more.
+          </p>
+          <p>
+            Engineers come to our hackathons, workshops and events to connect with each other & learn new technology.
+            Our community is known to expose its members to the newest ground-breaking frameworks, libraries, APIs and
+            SDKs – often before they achieve breakout popularity.
+          </p>
+        </section>
 
         {/*Testimonials*/}
-        <div className="bg-white mt-24 mx-auto" style={{ width: "60%", height: "300px" }}>
-          {" "}
-          TESTIMONIAL TODO
-        </div>
-      </Container>
-
-      {/* Curvy white div start*/}
-      <div className="bg-mb-blue-250 my-12 py-8">
-        <div>
-          <div className="w-full relative">
-            <img src={svgWhiteCurvedYTop} alt="" className="w-full relative" style={{ top: 2 }} />
+        <section>
+          <div className="bg-white mt-24 mx-auto" style={{ width: "60%", height: "300px" }}>
+            {" "}
+            TESTIMONIAL TODO
           </div>
-          <div className="bg-white text-center mx-auto py-16">
-            <H2>A snapshot of our community</H2>
+        </section>
+      </Container>
+      {/* Curvy white div start*/}
+      <section>
+        <div className="bg-mb-blue-250 my-12 py-8">
+          <div>
+            <div className="w-full relative">
+              <img src={svgWhiteCurvedYTop} alt="" className="w-full relative" style={{ top: 2 }} />
+            </div>
+            <div className="bg-white text-center mx-auto py-16">
+              <H2>A snapshot of our community</H2>
 
-            <div className="mt-10 grid gap-2 gap-y-8 grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 px-2 max-w-screen-md mx-auto">
-              {communityStats.map((item, i) => (
-                <Stat key={i} statColor="mint" stat={item.stat} label={item.label} className="px-4" centered />
-              ))}
+              <div className="mt-10 grid gap-2 gap-y-8 grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 px-2 max-w-screen-md mx-auto">
+                {communityStats.map((item, i) => (
+                  <Stat key={i} statColor="mint" stat={item.stat} label={item.label} className="px-4" centered />
+                ))}
+              </div>
+            </div>
+            <div className="w-full relative">
+              <img src={svgWhiteCurvedYBottom} alt="" className="w-full relative" style={{ top: -2 }} />
             </div>
           </div>
-          <div className="w-full relative">
-            <img src={svgWhiteCurvedYBottom} alt="" className="w-full relative" style={{ top: -2 }} />
-          </div>
         </div>
-      </div>
+      </section>
       {/*  Start text section "Partner with Mintbean" */}
       <Container className="mx-auto ">
-        <div className="my-12 ">
+        <section className="my-12 ">
           <div className="text-center">
             <H2>How we can help you</H2>
             <p>Here is how we can help you evangelize your company’s message.</p>
@@ -131,7 +149,7 @@ const Partners: FC = () => {
           <img
             src={svgPartnerPillars}
             className="mx-auto flex-grow"
-            style={{ maxHeight: "400px" }}
+            style={{ maxHeight: "450px" }}
             alt="Our four pillars supporting loyalty to your brand: Awareness, Branding, Repitition, Immersion "
           />
           <div className="grid gap-2 gap-y-8 grid-cols-1 xs:grid-cols-2">
@@ -143,12 +161,27 @@ const Partners: FC = () => {
               </div>
             ))}
           </div>
-        </div>
+        </section>
 
         {/* Contact form start*/}
-        <div className="max-w-screen-md my-12 mx-auto">
-          <PartnerContactFormEmailComponent />
-        </div>
+        <section>
+          <div className="max-w-screen-md my-12 mx-auto">
+            <PartnerContactFormEmailComponent />
+          </div>
+        </section>
+        {/* Previews sponors and partners logos */}
+        <section>
+          <div className="py-8">
+            <p>Previous Sponsors & Partners</p>
+            <div className="max-w-screen-md mx-auto my-8 grid gap-2 gap-y-8 grid-cols-2  sm:grid-cols-3 md:grid-cols-4">
+              {partnerLogos.map(({ logo, name }, i) => (
+                <div key={i} className="flex justify-center">
+                  <img src={logo} alt={`${name} logo`} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </Container>
     </div>
   );
