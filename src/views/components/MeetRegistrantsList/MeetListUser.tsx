8a00b6c8f53ef35543f0246890255195d36bf996
Dan Reardon <dreardon1021@gmail.com>
2020-12-23 11:41:08 -0500

    Adjust MeetListUser component for better conventio

--- src/views/components/MeetRegistrantsList/MeetListUser.tsx
+++ src/views/components/MeetRegistrantsList/MeetListUser.tsx
@@ -6,14 +6,17 @@ interface Props {
 }
 
 export const MeetListUser: FC<Props> = ({ meetRegistrant }) => {
+  const { firstName, lastName, createdAt } = meetRegistrant;
+
+  const fullName =`${firstName} ${lastName}`;
+  const memberSince = new Date(createdAt).getFullYear();
+
   return (
     <div className="flex align-center mt-6">
       <img src={greyProfilePic} />
       <div className="ml-3">
-        <p className="text-mb-blue-300 font-bold">
-          {meetRegistrant.firstName} {meetRegistrant.lastName}
-        </p>
-        <p className="text-white font-light">Member since {meetRegistrant.createdAt.split("-")[0]}</p>
+        <p className="text-mb-blue-300 font-bold">{fullName}</p>
+        <p className="text-white font-light">Member since {memberSince}</p>
       </div>
     </div>
   );
