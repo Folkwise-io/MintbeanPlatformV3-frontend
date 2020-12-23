f4629d30f6dd6ed4acb0448c59c6d8b03af89480
Dan Reardon <dreardon1021@gmail.com>
2020-12-23 11:59:58 -0500

    Prettier fix

--- src/views/components/MeetRegistrantsList/index.tsx
+++ src/views/components/MeetRegistrantsList/index.tsx
@@ -22,68 +22,68 @@ export const MeetRegistrantsList: FC<Props> = ({ meetRegistrants }) => {
 
   if (expanded) {
     containerView = (
-        <div className="flex flex-col justify-between bg-black min-w-lg rounded-lg max-h-32 p-4">
-          <div className="flex justify-between mb-6">
-            <div className="flex justify-between items-center w-2/5 h-auto">
-              <div className="flex h-8">
-                {meetRegistrants.slice(0, 3).map((registrant: RegistrantsForMeet, i: number) => {
-                  if (i === 0) {
-                    return (
-                      <div key={registrant.createdAt} className="w-6 overflow-visible">
-                        <img src={greyProfilePic} className="w-8 absolute" />
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div key={registrant.createdAt} className="w-6 overflow-visible">
-                        <img src={test} className="w-8 absolute" />
-                      </div>
-                    );
-                  }
-                })}
-              </div>
-              <p className="text-white font-bold">{meetRegistrants.length} Attendees</p>
+      <div className="flex flex-col justify-between bg-black min-w-lg rounded-lg max-h-32 p-4">
+        <div className="flex justify-between mb-6">
+          <div className="flex justify-between items-center w-2/5 h-auto">
+            <div className="flex h-8">
+              {meetRegistrants.slice(0, 3).map((registrant: RegistrantsForMeet, i: number) => {
+                if (i === 0) {
+                  return (
+                    <div key={registrant.createdAt} className="w-6 overflow-visible">
+                      <img src={greyProfilePic} className="w-8 absolute" />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={registrant.createdAt} className="w-6 overflow-visible">
+                      <img src={test} className="w-8 absolute" />
+                    </div>
+                  );
+                }
+              })}
             </div>
-            <p onClick={switchExpansion} className="text-mb-green-200 cursor-pointer hover:underline">
-              Hide All
-            </p>
+            <p className="text-white font-bold">{meetRegistrants.length} Attendees</p>
           </div>
-          <div className="overflow-y-scroll scrollbar h-96">
-            {meetRegistrants.map((r: RegistrantsForMeet) => {
-              return <MeetListUser key={r.id} meetRegistrant={r} />;
-            })}
-          </div>
+          <p onClick={switchExpansion} className="text-mb-green-200 cursor-pointer hover:underline">
+            Hide All
+          </p>
         </div>
+        <div className="overflow-y-scroll scrollbar h-96">
+          {meetRegistrants.map((r: RegistrantsForMeet) => {
+            return <MeetListUser key={r.id} meetRegistrant={r} />;
+          })}
+        </div>
+      </div>
     );
   } else {
     containerView = (
-        <div className="flex flex-col justify-between bg-black min-w-lg rounded-lg max-h-32 p-4">
-          <div className="flex justify-between">
-            <div className="flex justify-between items-center w-2/5 h-auto">
-              <div className="flex h-8">
-                {meetRegistrants.slice(0, 3).map((registrant: RegistrantsForMeet, i: number) => {
-                  if (i === 0) {
-                    return (
-                      <div className="w-6 overflow-visible">
-                        <img src={greyProfilePic} className="w-8 absolute" />
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div className="w-6 overflow-visible">
-                        <img src={test} className="w-8 absolute" />
-                      </div>
-                    );
-                  }
-                })}
-              </div>
-              <p className="text-white">{meetRegistrants.length} Attendees</p>
+      <div className="flex flex-col justify-between bg-black min-w-lg rounded-lg max-h-32 p-4">
+        <div className="flex justify-between">
+          <div className="flex justify-between items-center w-2/5 h-auto">
+            <div className="flex h-8">
+              {meetRegistrants.slice(0, 3).map((registrant: RegistrantsForMeet, i: number) => {
+                if (i === 0) {
+                  return (
+                    <div className="w-6 overflow-visible">
+                      <img src={greyProfilePic} className="w-8 absolute" />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div className="w-6 overflow-visible">
+                      <img src={test} className="w-8 absolute" />
+                    </div>
+                  );
+                }
+              })}
             </div>
-            <p onClick={switchExpansion} className="text-mb-green-200 cursor-pointer hover:underline">
-              Show All
-            </p>
+            <p className="text-white">{meetRegistrants.length} Attendees</p>
           </div>
+          <p onClick={switchExpansion} className="text-mb-green-200 cursor-pointer hover:underline">
+            Show All
+          </p>
         </div>
+      </div>
     );
   }
 
