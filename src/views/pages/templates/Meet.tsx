45cdb0cfcd65a7e3d10b18117468c8805491d8ac
Claire Froelich <claire.froelich@gmail.com>
2020-12-23 18:34:23 -0800

    Starting layout

--- src/views/pages/templates/Meet.tsx
+++ src/views/pages/templates/Meet.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { connect } from "react-redux";
 import { RouteComponentProps } from "react-router-dom";
 import { MeetRegistration } from "../../../utils/MeetRegistration";
+import { H2 } from "../../components/blocks/H2";
+import { Meet as IMeet } from "../../../types/meet";
 
 const meetReg = new MeetRegistration();
 
@@ -17,16 +19,50 @@ interface MatchParams {
   id: string;
 }
 
+const dummyContent = () => (
+  <>
+    <H2>Lorem</H2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam erat amet, in dui, est dolor, egestas ante. Eget
+      arcu etiam volutpat faucibus. Condimentum mi, nunc orci semper dictumst sed. Lectus nam leo, malesuada faucibus
+      proin. Integer lectus diam ultricies quis ut enim diam. Diam, fringilla mauris in amet.
+    </p>
+    <H2>Lorem</H2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam erat amet, in dui, est dolor, egestas ante. Eget
+      arcu etiam volutpat faucibus. Condimentum mi, nunc orci semper dictumst sed. Lectus nam leo, malesuada faucibus
+      proin. Integer lectus diam ultricies quis ut enim diam. Diam, fringilla mauris in amet.
+    </p>
+    <H2>Lorem</H2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam erat amet, in dui, est dolor, egestas ante. Eget
+      arcu etiam volutpat faucibus. Condimentum mi, nunc orci semper dictumst sed. Lectus nam leo, malesuada faucibus
+      proin. Integer lectus diam ultricies quis ut enim diam. Diam, fringilla mauris in amet.
+    </p>
+  </>
+);
+// For using react router 'match' prop
+interface MatchParams {
+  id: string;
+}
+
 const Meet: FC<StateMapping & RouteComponentProps<MatchParams>> = ({ user: userState, match }) => {
+  const [meet, setMeet] = useState<IMeet | null>(null);
   return (
-    <div className="mb-gradient-to-green-b min-h-screen p-1 md:px-12">
+    <div className="mb-gradient-to-green-b min-h-screen md:px-12">
       <div className="max-w-screen-lg mx-auto p-0 md:py-12">
-        <div className="mb-gradient-black-fade-b w-full h-64">
-          {/* Head */}
-          <div className="">
-            <div>
-              <div className="w-full h-full bg-gray-300">IMAGE</div>
+        <div className="mb-gradient-black-fade-b text-white font-light w-full rounded-lg">
+          <div className="flex">
+            {/* Left column */}
+            <div className="w-auto">
+              <div className="w-full h-full bg-gray-300 md:rounded-tl-lg" style={{ height: "200px" }}>
+                IMAGE
+              </div>
+
+              <div className="p-4 md:p-10 ">{dummyContent()}</div>
             </div>
+            {/* Right column*/}
+            <div></div>
           </div>
         </div>
       </div>
