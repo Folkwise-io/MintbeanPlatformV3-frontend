fc3c2b072b9ef966c6cf0800d7ee78834e8ad797
Claire Froelich <claire.froelich@gmail.com>
2020-12-24 12:59:53 -0800

    Tweak meet registrant list style a bit more

--- src/views/components/MeetRegistrantsList/index.tsx
+++ src/views/components/MeetRegistrantsList/index.tsx
@@ -59,7 +59,7 @@ export const MeetRegistrantsList: FC<Props> = ({ meetRegistrants }) => {
   const renderExpandedView = () => (
     <div>
       {renderSummaryHeader()}
-      <div className="flex flex-col justify-between bg-black rounded-b-lg max-h-32 px-4 pt-4">
+      <div className="flex flex-col justify-between bg-black rounded-b-lg max-h-32 px-4">
         <div className="overflow-y-scroll scrollbar h-96 pr-4">{meetRegistrants.map(renderMeetListUsers)}</div>
       </div>
     </div>
