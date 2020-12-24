b3e462e4bd485b6dda3768f03231303ca7ee5a93
Claire Froelich <claire.froelich@gmail.com>
2020-12-24 13:38:03 -0800

    Fix text area bug

--- src/views/components/blocks/Form/TextArea.tsx
+++ src/views/components/blocks/Form/TextArea.tsx
@@ -13,17 +13,17 @@ interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
 
 // must name function in forwardRef argument (non-arrow function) in order to avoid missing diplayName error
 export const TextArea = React.forwardRef<TextAreaProps, Props>(function textarea(
-  { className, name, label, srOnly = false, ...rest },
+  { className, name, label, isRequired, srOnly = false, ...rest },
   passedRef,
 ) {
   const { itemSpacing, inputStyles, labelStyles } = formConstants;
   const baseClasses = [itemSpacing, inputStyles, labelStyles, "whitespace-pre-wrap"].join(" ");
   const classes = appendOptionalClasses(baseClasses, className);
-
+  const theLabel = label + (isRequired ? "*" : "");
   return (
     <>
       <label className={labelStyles + ` ${srOnly ? "sr-only" : ""}`} htmlFor={name}>
-        {label}
+        {theLabel}
       </label>
       <textarea {...rest} name={name} ref={passedRef || null} className={classes} />
     </>
